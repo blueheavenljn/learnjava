4642ce659b4a9bae97c092d3ff8e25a42664028e
blueheavenljn <blueheavenljn@gmail.com>
2020-03-18 02:08:08 +0800

    Initial commit.

---
+++ src/main/java/com/chennan/learning/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.chennan.learning;
+
+/**
+ * @Auther: Jinan Liu
+ * @Date: 2020-03-18 02:02
+ * @Description:
+ */
+public class HelloWorld {
+
+    public static void main(String args[]) {
+        System.out.println("Hello World!");
+    }
+
+}
